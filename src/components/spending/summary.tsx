ccae55c43962f72815e297204572ca3dbbe146c2
Burak Haşıcı <burakhasici@gmail.com>
2023-12-13 22:48:44 +0300

    Animated number added

--- src/components/spending/summary.tsx
+++ src/components/spending/summary.tsx
@@ -1,13 +1,22 @@
+import AnimatedNumber from "react-animated-numbers";
 import "./index.scss";
 
-type Props = {};
-
-const Summary = (props: Props) => {
+const Summary = () => {
   return (
     <div className="summary-wrapper">
       <div className="summary-left-col">
         <small className="t">Total this month</small>
-        <p>$478.33</p>
+        <p className="total-animated-amount">
+          $
+          <AnimatedNumber
+            className=""
+            animateToNumber={478.33}
+            transitions={(index) => ({
+              type: "spring",
+              duration: index + 0.3,
+            })}
+          />
+        </p>
       </div>
       <div className="summary-right-col">
         <small>2.4%</small>
